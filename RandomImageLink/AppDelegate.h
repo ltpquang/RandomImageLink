980e703f9f7ac48690ba28ee63c549a26af55f3f
Quang Le <phuc.quang102@gmail.com>
2015-02-18 18:59:46 +0700

    Initial Commit

---
+++ RandomImageLink/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RandomImageLink
+//
+//  Created by Le Thai Phuc Quang on 2/18/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ RandomImageLink/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  RandomImageLink
+//
+//  Created by Le Thai Phuc Quang on 2/18/15.
+//  Copyright (c) 2015 QuangLTP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
